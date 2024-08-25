e7b4c728942e5aca869c8ddbcb5016b0fd2f0d45
Miles Padot <mpadot@emich.edu>
2024-08-24 21:42:44 -0400

    added color, and created new list for cars

--- src/Car.java
+++ src/Car.java
@@ -2,36 +2,53 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
+import java.util.ArrayList;
 
 public class Car {
-
+    private String color;
     public static void main(String[] args) {
-        Car car1 = new electricCar(1500);
-        Car car2 = new gasCar(5000);
-        Car waterCar = new waterCar(10);
+
+        ArrayList<Car> list = new ArrayList<Car>();
+        Car car1 = new electricCar(1500, "red");
+        list.add(car1);
+        Car car2 = new gasCar(5000, "Blue");
+        list.add(car2);
+        Car waterCar = new waterCar(10, "Green");
+        list.add(waterCar);
+
+
+
+
     }
 
 
 }
 
     class electricCar extends Car{
-        electricCar(Integer mielage){
-            System.out.println("Your Electric Car has " + mielage + " volts left on the battery");
-        }
+        electricCar(Integer mielage, String color){
+            System.out.println("Your Electric Car has " + mielage + " volts left on the battery. It is also "+ color);
+
+
+
+
+            }
+
+
+
     }
 
 
     class gasCar extends Car{
 
-        gasCar(Integer mielage){
-            System.out.println("Your Gas Car has " + mielage + " miles left in the tank");
+        gasCar(Integer mielage, String color){
+            System.out.println("Your Gas Car has " + mielage + " miles left in the tank. It is also "+ color);
         }
     }
 
 class waterCar extends Car{
 
-        waterCar(Integer mielage){
-            System.out.println("Your Water Car has " + mielage + " gallons of water left in the tank");
+        waterCar(Integer mielage, String color){
+            System.out.println("Your Water Car has " + mielage + " gallons of water left in the tank. It is also "+ color);
         }
     }
 
