df2ba099366f7abd7f310839318cfe3de1825978
Miles Padot <mpadot@emich.edu>
2024-08-24 21:42:44 -0400

    created a list and changed a bunch of stuff so it worked, which included making a new constructor int the super class, as well as overridiong the toString, because whenever I printed the objects out it would say the memory location rather than the actual data of the objects

--- src/Car.java
+++ src/Car.java
@@ -2,36 +2,75 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
+import java.util.ArrayList;
 
 public class Car {
+    private String color;
+    private Integer mielage;
+
+    public Car(Integer mielage, String color){
+        this.color = color;
+        this.mielage = mielage;
+    }
+    public String toString() {
+        return "Mileage: " + mielage + " mpg, the color of the car is: " + color;
+    }
+
+
 
     public static void main(String[] args) {
-        Car car1 = new electricCar(1500);
-        Car car2 = new gasCar(5000);
-        Car waterCar = new waterCar(10);
+
+        ArrayList<Car> list = new ArrayList<Car>();
+        Car car1 = new electricCar(1500, "red");
+        list.add(car1);
+        Car car2 = new gasCar(5000, "Blue");
+        list.add(car2);
+        Car waterCar = new waterCar(10, "Green");
+        list.add(waterCar);
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+
+
     }
 
 
 }
 
     class electricCar extends Car{
-        electricCar(Integer mielage){
-            System.out.println("Your Electric Car has " + mielage + " volts left on the battery");
-        }
+        public electricCar(Integer mielage, String color){
+            super(mielage,color);
+
+            }
+            public String toString(){
+                return "Your Electric Car has a " + super.toString() ;
+
+            }
+
+
     }
 
 
     class gasCar extends Car{
 
-        gasCar(Integer mielage){
-            System.out.println("Your Gas Car has " + mielage + " miles left in the tank");
+        public gasCar(Integer mielage, String color){
+            super(mielage, color);
+        }
+        public String toString(){
+            return"Your Water Car has a" + super.toString()  ;
+
         }
     }
 
-class waterCar extends Car{
+    class waterCar extends Car{
 
-        waterCar(Integer mielage){
-            System.out.println("Your Water Car has " + mielage + " gallons of water left in the tank");
+        public waterCar(Integer mielage, String color){
+            super(mielage, color);
+        }
+        public String toString(){
+            return "Your Water Car has a" + super.toString();
         }
     }
 
